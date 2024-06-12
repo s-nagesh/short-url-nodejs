d84eb9599b5e370da44c2dcc959af00548c90bf2
Nagesh bhimashankar swami <75519023+s-nagesh@users.noreply.github.com>
2024-06-12 16:50:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const userRoute = require("./routes/user");
 const app = express();
 const PORT = 8001;
 
-connectToMongoDB(process.env.MONGODB ?? "mongodb://127.0.0.1:27017/short-url").then(() =>
+connectToMongoDB("mongodb+srv://snagesh27:nagesh2727@cluster0.c3bidtm.mongodb.net/short-url").then(() =>
   console.log("Mongodb connected")
 );
 
