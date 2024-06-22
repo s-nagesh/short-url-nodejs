551f47ffef7996d80d317e73a94381010d17907d
Nagesh bhimashankar swami <nagesh.swami209@gmail.com>
2024-06-22 16:23:06 +0530

    remove console

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,6 @@ const User = require("../models/user");
 const { setUser } = require("../service/auth");
 
 async function handleUserSignup(req, res) {
-  console.log("req", req.body);
   const { name, email, password } = req.body;
   await User.create({
     name,
