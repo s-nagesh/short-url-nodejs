0cc047570486e22aa08e4ae184cb92408ad0eec0
Nagesh bhimashankar swami <nagesh.swami209@gmail.com>
2024-06-22 16:31:57 +0530

    remove console

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/url", restrictToLoggedinUserOnly, urlRoute);
-app.use("/user", userRoute);
+app.use("/api/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/api/user", userRoute);
 app.use("/", checkAuth, staticRoute);
 
 app.get("/url/:shortId", async (req, res) => {
